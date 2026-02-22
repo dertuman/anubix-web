ad5a5eba383e6688365275f44fd625ca98e13d04
Alexander T. Karslake <alex911_911@hotmail.com>
2026-02-22 13:08:03 -0600

    feat: remove google analytics

--- components/analytics.tsx
+++ components/analytics.tsx
@@ -1,19 +1,5 @@
-'use client';
-
 import { Analytics as VercelAnalytics } from '@vercel/analytics/next';
-import { GoogleAnalytics, GoogleTagManager } from '@next/third-parties/google';
-
-import {
-  NEXT_PUBLIC_GOOGLE_ANALYTICS_ID,
-  NEXT_PUBLIC_GTM_ID,
-} from '@/lib/constants';
 
 export function Analytics() {
-  return (
-    <>
-      <GoogleTagManager gtmId={NEXT_PUBLIC_GTM_ID} />
-      <GoogleAnalytics gaId={NEXT_PUBLIC_GOOGLE_ANALYTICS_ID} />
-      <VercelAnalytics />
-    </>
-  );
+  return <VercelAnalytics />;
 }
--- lib/constants.ts
+++ lib/constants.ts
@@ -4,10 +4,6 @@ export const API_BASE_URL = process.env.API_BASE_URL || '';
 export const PORT = process.env.PORT || '';
 
 
-export const NEXT_PUBLIC_GOOGLE_ANALYTICS_ID =
-  process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID || '';
-export const NEXT_PUBLIC_GTM_ID = process.env.NEXT_PUBLIC_GTM_ID || '';
-
 export const TESTING_EMAIL = process.env.TESTING_EMAIL || '';
 export const TESTING_PASSWORD = process.env.TESTING_PASSWORD || '';
 export const TESTING_EMAIL_2 = process.env.TESTING_EMAIL_2 || '';
